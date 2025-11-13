e645c11561fbb43b66fadbe67611f0ddb14b3db4
Nagy Máté <morsakk.mate@gmail.com>
2025-11-13 12:08:55 +0100

    new vcf and working email sender

---
+++ script.js
@@ -0,0 +1,22 @@
+emailjs.init("7FLSkJKbCg6jSUBeu");
+
+const form = document.getElementById("contact-form");
+const senderName = document.getElementById("user_name")
+const senderEmail = document.getElementById("user_email")
+const content = document.getElementById("message")
+const status = document.getElementById("form-status");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  emailjs
+    .sendForm("service_66wc4fj", "template_5yzxe7a", form)
+    .then(() => {
+      status.textContent = "✅ Üzeneted sikeresen elküldve!";
+      status.style.color = "#00ffb3";
+      form.reset();
+    })
+    .catch((e) => {
+      status.textContent = "❌ Hiba történt az üzenet küldése közben.";
+      status.style.color = "#ff6b6b";
+    });
+});
